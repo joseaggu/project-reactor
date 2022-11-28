9ef603a0d3cff3a327c7f2852d9a00a57cd94925
Jose Andres <joseaggu@ext.inditex.com>
2022-11-28 13:53:01 -0500

    Add more examples

--- src/test/java/com/ownpractice/FluxAndMonoFlowTest.java
+++ src/test/java/com/ownpractice/FluxAndMonoFlowTest.java
@@ -1,36 +1,76 @@
 package com.ownpractice;
 
-import jdk.jfr.Description;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 class FluxAndMonoFlowTest {
 
-  @Test
-  @Description("Example Flux with StepVerifier")
-  void name() {
-    //when
-    var result = FluxAndMonoFlow.colorsMono;
-
-    //then
-    StepVerifier.create(result)
-        .expectNext("red")
-        .verifyComplete();
-
+  private static List<String> getColors() {
+    return List.of("red", "blue", "black");
   }
 
   @Test
-  @Description("Example Mono with StepVerifier")
-  void namesMono() {
+  void name() {
+    StepVerifier.create(Mono.just("red").log())//Similar to how get data from DB or Service
+        .expectNext("red")
+        .verifyComplete();
+  }
+
+  @Test
+  void fluxAllMethods() {
+    StepVerifier.create(Flux.fromIterable(getColors()).log())// Similar to how get data from DB or Service
+        .expectNext("red", "blue", "black")
+        .verifyComplete();
+  }
+
+  @Test
+  void fluxMap() {
+    //given
+    var fluxMethod = Flux.fromIterable(getColors()).log();
 
     //when
-    var namesMono = Mono.just("alex");
+    var actual = fluxMethod
+        .map(String::toUpperCase);
 
     //then
-    StepVerifier.create(namesMono)
-        .expectNext("alex")
-        .expectNextCount(0)
+    StepVerifier.create(actual)
+        .expectNext("RED", "BLUE", "BLACK")
         .verifyComplete();
+  }
+
+  @Test
+  void filterFlux() {
+    //given
+    var fluxMethod = Flux.fromIterable(getColors());
+
+    //when
+    var actual = fluxMethod.filter(it -> it.length() <= 4).log();
+
+    //then
+    StepVerifier.create(actual)
+        .expectNext("red", "blue")
+        .verifyComplete();
+  }
+
+  @Test
+  void homeWork() {
+
+    //when
+    var actual = namesMono_map_filter(3).log();
+
+    //then
+    StepVerifier.create(actual)
+        .expectNext("ALEX")
+        .verifyComplete();
+  }
+
+  private Mono<String> namesMono_map_filter(int stringLength) {
+    return Mono.just("alex")
+        .filter(it -> it.length() > stringLength)
+        .map(String::toUpperCase);
   }
 }
